e01ebd3fddbe38dbf61f7e5a23add3b53bbbadbe
mcfk891 <57649594+mcfk891@users.noreply.github.com>
2019-12-12 16:23:10 +0800

    prac_05

--- state_names.py
+++ state_names.py
@@ -5,14 +5,15 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-# print(STATE_NAMES)
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia","ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 
-state = input("Enter short state: ")
-while state != "":
+for short_state, long_state in STATE_NAMES.items():
+    print("{:3} is {:3}".format(short_state,long_state))
+
+state = input("Enter short state: ").upper()
+while state != " ":
     if state in STATE_NAMES:
         print(state, "is", STATE_NAMES[state])
     else:
         print("Invalid short state")
-    state = input("Enter short state: ")
+    state = input("Enter short state: ").upper()
